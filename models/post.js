e733091807d737705ca54f5e6023000f8fff95a3
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-06 17:25:34 +0100

    tittle aded

--- models/post.js
+++ models/post.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose'); 
 
+const postShema  = new mongoose.Shema ({
+    tittle : { 
+        type : String ,  
+        required  : true , 
+        trim :  true ,  
+
+    }, 
+
+    description  : {
+        
+    }
+})
