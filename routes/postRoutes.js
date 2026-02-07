e250896b2dfd82c1725b1e581683acfaed9afced
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-07 22:56:22 +0100

    commit

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,8 +2,15 @@ const express = require(express);
 const Post = require("/models/post.js"); 
 
 const router = express.Router();
-const router = express.Router();
-const router = express.Router();
-const router = express.Router();
-const router = express.Router();
 
+
+// creat a post 
+router.post("/",  async  (req,res) => {
+    try{
+        const post =  awaiit  Post.create(req.body);
+        
+    }
+    catch(error) {
+
+    }a
+});
