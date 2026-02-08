40464a73e04b588f452525d181d01e2898ed2cef
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-08 16:42:48 +0100

    issue

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express  = require("express"); 
 const connectDB = require("./config/db.js"); 
 const dotenv = require("dotenv");
-const postRoues = require("./routes/postRoutes.js");
+const postRoutes = require("./routes/postRoutes.js");
 
 dotenv.config(); 
 connectDB();  
-
+const app = express();   
 app.use(express.json());
 
 
-const app = express();   
+
 
 app.get("/", (req, res)  => {
     res.send ("LF api is runnig "); 
 }); 
 
 
-app.use("/api/posts", postRoues);
-const  PORT =  5000 ;  
+app.use("/api/posts", postRoutes);
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,  () =>  {
     console.log('server is running on port 5000'); 
 });
