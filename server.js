f41227e635f35eba21a0616aaff157de2e7f04f1
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-08 16:11:49 +0100

    app.uuse

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express  = require("express"); 
 const connectDB = require("./config/db.js"); 
 const dotenv = require("dotenv");
+const postRoues = require("./routes/postRoutes.js");
 
 dotenv.config(); 
 connectDB();  
 
+app.use(express.json());
 
 
 const app = express();   
@@ -13,6 +15,8 @@ app.get("/", (req, res)  => {
     res.send ("LF api is runnig "); 
 }); 
 
+
+app.use("/api/posts", postRoues);
 const  PORT =  5000 ;  
 app.listen(PORT,  () =>  {
     console.log('server is running on port 5000'); 
