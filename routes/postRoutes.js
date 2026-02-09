8360636e39a42620dbe1ea64b926507c10c45b07
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-09 21:02:06 +0100

    commit

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -26,7 +26,11 @@ router.get("/", async (req, res) => {
 // Get a post by id 
 router.get("/:id", async(req, res) =>{
     try {
-
+        const post = await Post.findById(req.params.id); 
+        if(!post) {
+            return res.status(404).json({ message  : "Post not found "});
+        }
+        res.json(post);
     }
     catch (error) {
         
